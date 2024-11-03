a94585c168ac5320a50bf03db42565b27c325c1d
Avashneupane9857 <juisch20220011252@gmail.com>
2024-11-04 04:06:28 +0530

    feat: turboSetup

--- my-turborepo/apps/httpServer/src/index.ts
+++ my-turborepo/apps/httpServer/src/index.ts
@@ -1,7 +1,9 @@
 import express from "express"
+import client from "@repo/db/client"
 import { route } from "./router/route"
 const app=express()
 const port =3000
+
 app.use("/api/v1",route)
 app.listen(process.env.PORT || port,()=>{
     console.log(`listening to port ${port}`)
